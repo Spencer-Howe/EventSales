60db9792798f373cc5617109456f639cf7423a64
Spencer Howe <spenceralanhowe@gmail.com>
2025-12-02 20:53:55 -0800

    fixed run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from eventapp import create_app
-from eventapp.tasks import check_and_send_reminders, clear_old_bookings
+from eventapp.tasks import check_and_send_reminders
 
 
-app = create_app(check_and_send_reminders=check_and_send_reminders, clear_old_bookings=clear_old_bookings)
+app = create_app(check_and_send_reminders=check_and_send_reminders, clear_old_bookings=None)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
